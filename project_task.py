3cf5f163743c5edbc36b95178ae706722fa04a60
seiya_namba <sn@seiya-nambanoMacBook-Pro.local>
2023-11-21 21:55:31 +0900

    some miner change

--- project_task.py
+++ project_task.py
@@ -18,23 +18,23 @@ class Task_manager:
 
     def __init__(self):
         self.task_list = []
+        #   self.task_dict = {}
 
     def task_add(self, task_title, task_description, task_detail, task_status):
         task = Task(task_title, task_description, task_detail, task_status)
         self.task_list.append(task)
+        # self.task_dict[task] = task
     
     def count_task(self):
         return len(self.task_list)
 
-    def New_status(self,new_status):
+    def New_status(self,task_number,new_status):
         if new_status not in Task.valid_values:
             raise ValueError("無効な値です。")
         else:
-            self.new_status = new_status
-            return self.new_status
+            self.task_list[task_number] = new_status
         
-        # if new_status == "DONE":
-        #     Task_manager.number_of_task_done += 1
+# Task_manager.number_of_task_done += 1
     
     def status_update(self,update_task):
         pass
@@ -50,13 +50,13 @@ print("========================")
 #初期化コード、初期化していないと__init__内の定義が機能しない
 task_zero = Task_manager()
 task_zero.task_add("presentaiton","aaaa","presentation","TO_DO")
+task_zero.task_add("presentaitonb","bbbb","nnnnn","TO_DO")
+
 task_count_zero = task_zero.count_task()
 all_tasks = task_zero.get_all_tasks()
-# task_show = Task.__repr__()
 
 print(task_count_zero)
 print(all_tasks)
-
 print(Task.valid_values)
 
 
@@ -71,11 +71,12 @@ else:
 print(dir(Task_manager))
 
 #特定のタスクのステータスをアップデートして表示
-task_zero_new_status = task_zero.New_status("DONE")
+task_zero_new_status = task_zero.New_status(0,"DONE")
 print(task_zero_new_status)
 
-all_tasks[0].task_status = task_zero_new_status
-print(all_tasks[0].task_status)
+
+# all_tasks[0].task_status = task_zero_new_status
+# print(all_tasks[0].task_status)
 
 print("確認用のコードはここまでです。")
 print("========================")
@@ -87,10 +88,10 @@ print("========================")
 
 
 #最初のタスクを作成します
-one_title = input("task_name here")
-one_description = input("what is the description of this Task ? ")
-one_task_detail = input("please write here detail of this Task ")
-one_task_status = input("Already  this task is decided to do ? ")
+one_title = input("task_name here : ")
+one_description = input("what is the description of this Task ? : ")
+one_task_detail = input("please write here detail of this Task : ")
+one_task_status = input("Already  this task is decided to do ? : ")
 
 task_one = Task_manager()
 task_one.task_add(one_title,one_description,one_task_detail,one_task_status)
@@ -104,10 +105,10 @@ else:
 
 
 #二つ目のタスクを作成します
-two_title = input("task_name here")
-two_description = input("what is the description of this Task ? ")
-two_task_detail = input("please write here detail of this Task ")
-two_task_status = input("Already  this task is decided to do ? ")
+two_title = input("task_name here : ")
+two_description = input("what is the description of this Task ? : ")
+two_task_detail = input("please write here detail of this Task : ")
+two_task_status = input("Already  this task is decided to do ? : ")
 
 
 task_one.task_add(two_title,two_description,two_task_detail,two_task_status)
@@ -124,12 +125,12 @@ current_all_task = task_one.get_all_tasks()
 #すべてのタスクの現在のステータスを表示します。
 
 for i, task in enumerate(current_all_task):
-    print("タスク" + current_all_task[i].task_title + "のステータスは[" + current_all_task[i].task_status+ "] です。")
+    print("タスク" + current_all_task[i].task_title + " のステータスは[" + current_all_task[i].task_status+ "] です。")
 
 
-print("task_status update is needed?") 
+print("task_status update is needed ? : ") 
 task_one_update_target = input("which task?")
-task_one_update = input("what is the new status of this task?") 
-
+task_one_update = input("what is the new status of this task ?  : ") 
+ 
 current_all_task[int(task_one_update_target)].task_status = task_one_update
-print("タスク" + current_all_task[int(task_one_update_target)].task_title + "の新しいステータスは[" + task_one_update + "] です。")
+print("タスク" + current_all_task[int(task_one_update_target)].task_title + " の新しいステータスは[" + task_one_update + "] です。")
